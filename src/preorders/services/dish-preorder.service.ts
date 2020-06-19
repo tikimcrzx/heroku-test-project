0af28fdad3482266e09d48117cf1967be2b83f06
tikimcrzx <tikimioo723@gmail.com>
2020-06-18 19:52:42 -0700

    [Fix DishPreorder add populate]

--- src/preorders/services/dish-preorder.service.ts
+++ src/preorders/services/dish-preorder.service.ts
@@ -1,23 +1,22 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { DishPreOrder } from "../models/dish-preorder.model";
-import { CreateDishPreOrderDTO } from "../input-dto";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DishPreOrder } from '../models/dish-preorder.model';
+import { CreateDishPreOrderDTO } from '../input-dto';
 
 @Injectable()
 export class DishPreOrderService {
   constructor(
-    @InjectModel("DishPreOrder") private readonly _dishPreOrder: Model<
-      DishPreOrder
-    >,
+    @InjectModel('DishPreOrder')
+    private readonly _dishPreOrder: Model<DishPreOrder>,
   ) {}
 
   async create(
     createDishPreOrder: CreateDishPreOrderDTO,
   ): Promise<DishPreOrder> {
-    const duplicated: DishPreOrder = await this._dishPreOrder.findOne(
-      { dish: createDishPreOrder.dish },
-    );
+    const duplicated: DishPreOrder = await this._dishPreOrder.findOne({
+      dish: createDishPreOrder.dish,
+    });
 
     let dishPreOrder: DishPreOrder;
 
@@ -40,13 +39,29 @@ export class DishPreOrderService {
   }
 
   async findById(id: string): Promise<DishPreOrder> {
-    const dishPreOrder: DishPreOrder = await this._dishPreOrder.findById(id);
+    const dishPreOrder: DishPreOrder = await this._dishPreOrder
+      .findById(id)
+      .select('-createdAt -updatedAt -__v')
+      .populate({
+        path: 'dish',
+        model: 'Dish',
+        select: '-createdAt -updatedAt -__v',
+        populate: { path: 'ingredients', model: 'Ingredient', select: 'name' },
+      });
     if (!dishPreOrder) throw new NotFoundException(`Pre Order ${id} not found`);
     return dishPreOrder;
   }
 
   async findAll(): Promise<DishPreOrder[]> {
-    const dishPreOrders: DishPreOrder[] = await this._dishPreOrder.find();
+    const dishPreOrders: DishPreOrder[] = await this._dishPreOrder
+      .find()
+      .select('-createdAt -updatedAt -__v')
+      .populate({
+        path: 'dish',
+        model: 'Dish',
+        select: '-createdAt -updatedAt -__v',
+        populate: { path: 'ingredients', model: 'Ingredient', select: 'name' },
+      });
     return dishPreOrders;
   }
 }
