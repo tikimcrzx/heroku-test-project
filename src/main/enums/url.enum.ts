8504d9a972b987124b12832ded6af950fa9f1a77
tikimcrzx <tikimioo723@gmail.com>
2020-06-23 22:34:41 -0700

    [Update Terminara]

--- src/main/enums/url.enum.ts
+++ src/main/enums/url.enum.ts
@@ -2,5 +2,5 @@ export enum URLs {
   MENU = 'branch/menu',
   ORDER = 'dish/order',
   MAXORDER = 'branch/maxorder',
-  FINISH = 'https://analiticass.herokuapp.com/api/v1/dishpreorder/finish',
+  FINISH = 'dishpreorder/finish',
 }
